7d11f151a2924309d8ab3601cbc4c6a8cd6b12e8
Alexander K. Hudek <alexander@hudek.org>
2014-04-17 20:59:45 -0400

    Fix version number.

--- project.clj
+++ project.clj
@@ -1,10 +1,8 @@
-(defproject edl "0.1.0-SNAPSHOT"
-  :description "A demonstration of how to extract a schema from an sql database and
-  use it to check and generate sql."
+(defproject edl "0.1.0"
+  :description "A library for working with sql database schemas."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/java.jdbc "0.3.2"]
+  :dependencies [[org.clojure/java.jdbc "0.3.2"]
                  [environ "0.4.0"]]
   :profiles {:dev {:dependencies [[org.postgresql/postgresql "9.2-1003-jdbc4"]]}})
